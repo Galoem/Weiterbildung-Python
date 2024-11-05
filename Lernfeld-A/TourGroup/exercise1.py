887a46d7a25f18205c4ac7d3ed8b46b520bed6b4
Galoem <julianmichaelis@web.de>
2024-11-05 13:18:18 +0100

    add exercise1.py

---
+++ Lernfeld-A/TourGroup/exercise1.py
@@ -0,0 +1,40 @@
+# exercise 1
+
+PI = 3.141592653589793
+
+sideA = 2
+sideB = 5
+sideC = 3
+height = 7
+radius = 4
+diameter = radius * 2
+
+# Aufgabe 1: Addition zweier Zahlen
+print(f"Addition {sideA} + {sideB} = {sideA + sideB}")
+
+# Aufgabe 2: Subtraktion zweier Zahlen
+print(f"Subtraktion {sideA} - {sideB} = {sideA - sideB}")
+
+# Aufgabe 3: Multiplikation zweier Zahlen
+print(f"Multiplikation {sideA} * {sideB} = {sideA * sideB}")
+
+# Aufgabe 4: Division zweier Zahlen
+print(f"Division {sideB} / {sideA} = {sideB / sideA:.2f}")
+
+# Aufgabe 5: Berechnung des Durchschnitts von drei Zahlen
+print(f"Durchschnitt von {sideA}, {sideB}, {sideC} = {(sideA + sideB + sideC) / 2:.2f}")
+
+# Aufgabe 6: Berechnung des Flächeninhalts eines Rechtecks
+print(f"Fläche Rechteck {sideA} x {sideB} = {sideA * sideB:.2f}")
+
+# Aufgabe 7: Berechnung des Umfangs eines Rechtecks
+print(f"Umfang Rechteck {sideA} x {sideB} = {2 * sideA + 2 * sideB:.2f}")
+
+# Aufgabe 8: Berechnung des Flächeninhalts eines Dreiecks
+print(f"Fläche Dreieck über Seitenlänge {sideB} und Höhe {sideA} = {(sideA * height) / 2:.2f}")
+
+# Aufgabe 9: Berechnung des Umfangs eines Kreises
+print(f"Umfang Kreis mit Durchmesser {diameter} = {PI * diameter:.2f}")
+
+# Aufgabe 10: Berechnung des Flächeninhalts eines Kreises
+print(f"Umfang Kreis mit Radius {radius} = {PI*(radius**2):.2f}")
